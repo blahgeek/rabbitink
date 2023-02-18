f01a0c0e1b2c48f5c831f12dd277aa73f162ad92
Yikai Zhao <yikai@z1k.dev>
2023-02-19 00:23:56 +0800

    xcb: support cursor and screenshot

--- src/source/xcbgrab.rs
+++ src/source/xcbgrab.rs
@@ -30,6 +30,10 @@ impl Shmem {
         self.size
     }
 
+    fn mut_slice(&self) -> &mut [u8] {
+        unsafe { std::slice::from_raw_parts_mut(self.data as *mut u8, self.size) }
+    }
+
     fn slice(&self) -> &[u8] {
         unsafe { std::slice::from_raw_parts(self.data as *const u8, self.size) }
     }
@@ -53,6 +57,14 @@ pub struct XcbGrabSource {
 
     top_left: Point,
     size: Size,
+
+    screensave_img: ImageBuffer<32>,
+}
+
+fn make_screensave_img(size: Size) -> ImageBuffer<32> {
+    let mut img = ImageBuffer::<32>::new(size.width, size.height, None);
+    img.fill(0xff);
+    return img
 }
 
 impl XcbGrabSource {
@@ -61,6 +73,11 @@ impl XcbGrabSource {
         let setup = conn.get_setup();
         let screen = setup.roots().nth(screen_num as usize).unwrap();
         let window = screen.root();
+
+        let xfixes_query_cookie = conn.send_request(&xcb::xfixes::QueryVersion {
+            client_major_version: xcb::xfixes::MAJOR_VERSION,
+            client_minor_version: xcb::xfixes::MINOR_VERSION,
+        });
 
         let geo_cookie = conn.send_request(&xcb::x::GetGeometry {
             drawable: xcb::x::Drawable::Window(window),
@@ -101,12 +118,71 @@ impl XcbGrabSource {
             shmem,
             top_left,
             size,
+            screensave_img: make_screensave_img(size),
         })
     }
+
+    fn draw_cursor(
+        &self,
+        img: &mut impl Image<32>,
+        cursor_image: xcb::xfixes::GetCursorImageReply,
+    ) {
+        let cx = cursor_image.x() as i32 - cursor_image.xhot() as i32;
+        let cy = cursor_image.y() as i32 - cursor_image.yhot() as i32;
+
+        let x = i32::max(cx, self.top_left.x);
+        let y = i32::max(cy, self.top_left.y);
+        let w = i32::min(
+            cx + cursor_image.width() as i32,
+            self.top_left.x + self.size.width,
+        ) - x;
+        let h = i32::min(
+            cy + cursor_image.height() as i32,
+            self.top_left.y + self.size.height,
+        ) - y;
+
+        for j in 0..h {
+            let c_ptr = unsafe {
+                cursor_image.cursor_image().as_ptr().add(
+                    cursor_image.width() as usize * (j + (y - cy)) as usize + (x - cx) as usize,
+                )
+            };
+            let i_ptr = unsafe {
+                let p = img.mut_ptr(j + y - self.top_left.y).cast::<u32>();
+                p.add((x - self.top_left.x) as usize)
+            };
+            for i in 0..w {
+                unsafe {
+                    let cursor_rgba = *c_ptr.add(i as usize);
+                    *i_ptr.add(i as usize) = blend_rgba(cursor_rgba, *i_ptr.add(i as usize));
+                }
+            }
+        }
+    }
+}
+
+fn blend_rgba(cursor: u32, img: u32) -> u32 {
+    let alpha = (cursor >> 24) & 0xff;
+    return blend(cursor & 0xff, img & 0xff, alpha)
+        | (blend((cursor >> 8) & 0xff, (img >> 8) & 0xff, alpha) << 8)
+        | (blend((cursor >> 16) & 0xff, (img >> 16) & 0xff, alpha) << 16);
+}
+
+fn blend(cursor: u32, img: u32, alpha: u32) -> u32 {
+    (cursor + (img * (255 - alpha) + 255 / 2) / 255).clamp(0, 255)
 }
 
 impl Source for XcbGrabSource {
     fn get_frame(&mut self) -> anyhow::Result<ConstImageView<32>> {
+        let screensaver_query_cookie = self.conn.send_request(&xcb::screensaver::QueryInfo {
+            drawable: xcb::x::Drawable::Window(self.window),
+        });
+        let screensaver_queryinfo = self.conn.wait_for_reply(screensaver_query_cookie)?;
+        trace!("got screensaver info: {:?}", screensaver_queryinfo);
+        if screensaver_queryinfo.state() == xcb::screensaver::State::On as u8 {
+            return Ok(self.screensave_img.view());
+        }
+
         let image_cookie = self.conn.send_request(&xcb::shm::GetImage {
             drawable: xcb::x::Drawable::Window(self.window),
             x: self.top_left.x as i16,
@@ -121,6 +197,23 @@ impl Source for XcbGrabSource {
         let image = self.conn.wait_for_reply(image_cookie)?;
         trace!("got image: {:?}", image);
         assert_eq!(image.size() as usize, self.shmem.size());
+
+        let cursor_cookie = self.conn.send_request(&xcb::x::QueryPointer {
+            window: self.window,
+        });
+        let cursor = self.conn.wait_for_reply(cursor_cookie)?;
+        trace!("got cursor: {:?}", cursor);
+        if cursor.same_screen() {
+            let cursor_image_cookie = self.conn.send_request(&xcb::xfixes::GetCursorImage {});
+            let cursor_image = self.conn.wait_for_reply(cursor_image_cookie)?;
+            let mut image = ImageView::<32>::new(
+                self.shmem.mut_slice(),
+                self.size.width,
+                self.size.height,
+                Some(self.size.width * 4),
+            );
+            self.draw_cursor(&mut image, cursor_image);
+        }
 
         Ok(ConstImageView::<32>::new(
             self.shmem.slice(),
